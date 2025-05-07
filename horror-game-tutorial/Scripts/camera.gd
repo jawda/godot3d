7fa3dc1d9f559111aab25e6b51a8320152bcb159
Joe Williams <joew@quavermusic.com>
2025-05-07 13:00:58 -0500

    feat: started creating level with lighting and added material to walls and floors

--- horror-game-tutorial/Scripts/camera.gd
+++ horror-game-tutorial/Scripts/camera.gd
@@ -9,6 +9,7 @@ func _ready() -> void:
 	pass
 	
 func _input(event: InputEvent) -> void:
+	
 	if event is InputEventMouseMotion:
 		#get player object rotate based on mouse movement
 		get_parent().rotate_y(-event.relative.x * sens)
@@ -16,4 +17,9 @@ func _input(event: InputEvent) -> void:
 		rotate_x(-event.relative.y * sens)
 		#control how far player can see
 		rotation.x = clamp(rotation.x, deg_to_rad(-90), deg_to_rad(90))
+	
+	#temp fix to allow quitting the game probably a better way
+	if event.is_action_pressed("ui_cancel"): # Escape key
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE) # Release capture
+	 
 	pass
--- horror-game-tutorial/Scripts/player.gd
+++ horror-game-tutorial/Scripts/player.gd
@@ -1,31 +1,51 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
+var ORIGINAL_SPEED
+var SPEED = 3.0
+var SPRINT_SPEED = 7.0
 const JUMP_VELOCITY = 4.5
+var sprint_slider
+var sprint_drain_amount = 0.2
+var sprint_refresh_amount = 0.4
 
-
+func _ready() -> void:
+	ORIGINAL_SPEED = SPEED
+	sprint_slider = get_node("/root/" + get_tree().current_scene.name + "/UI/sprint_slider")
+	
+func _process(delta: float) -> void:
+	if SPEED == SPRINT_SPEED:
+		sprint_slider.value = sprint_slider.value - sprint_drain_amount * delta
+		#print("value is at:" + str(sprint_slider.value))
+		if sprint_slider.value <= sprint_slider.min_value:
+			SPEED = ORIGINAL_SPEED
+	if SPEED != SPRINT_SPEED:
+		if sprint_slider.value < sprint_slider.max_value: 
+			sprint_slider.value = sprint_slider.value + sprint_refresh_amount * delta
+		if sprint_slider.value == sprint_slider.max_value:
+			sprint_slider.visible = false
+				
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		if Input.is_action_just_pressed("sprint"):
+			sprint_slider.visible = true
+			SPEED = SPRINT_SPEED
+			
+		if Input.is_action_just_released("sprint"):
+			SPEED = ORIGINAL_SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
 	
-	if Input.get_vector("ui_cancel","","",""):
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
